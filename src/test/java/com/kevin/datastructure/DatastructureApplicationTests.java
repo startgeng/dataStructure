415372b692460d5a01e2c800d0f1f773dc3e8610
startgeng <2447925377@qq>
2020-10-14 20:51:33 +0800

    数据结构的学习和练习  2020-10-14 20:51

---
+++ src/test/java/com/kevin/datastructure/DatastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.datastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
